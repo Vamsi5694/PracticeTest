d8ce8538396d59ae17e8642c051d3d32d4cccf27
Vamsi5694 <45565430+Vamsi5694@users.noreply.github.com>
2020-11-29 17:21:13 +0530

    Added remote driver

--- src/test/java/com/execution/AmazonTest.java
+++ src/test/java/com/execution/AmazonTest.java
@@ -2,14 +2,19 @@
 package com.execution;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
@@ -22,14 +27,20 @@ public class AmazonTest {
 	WebDriver driver = null;
 	@BeforeSuite
 	@Parameters({"browserName"})
-	public void setup(String browser) {
+	public void setup(String browser) throws MalformedURLException {
 		if(browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			DesiredCapabilities dec = DesiredCapabilities.chrome();
+			dec.setCapability("version", "");
+			dec.setPlatform(Platform.WINDOWS);
+			driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), dec);
 		}
 		else if(browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			DesiredCapabilities dec = DesiredCapabilities.firefox();
+			dec.setCapability("version", "");
+			dec.setPlatform(Platform.WINDOWS);
+			driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), dec);
 		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
